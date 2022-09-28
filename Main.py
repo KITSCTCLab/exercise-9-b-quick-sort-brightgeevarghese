68ed4c45d93ee10041ce984ac9a163a2876e7bf6
Bright Gee Varghese <brightlsom@gmail.com>
2022-09-28 12:32:46 +0530

    Initial commit

--- Main.py
+++ Main.py
@@ -1,8 +1,37 @@
 from typing import List
 
+def partition(array, low, high):
+    # Choose the rightmost element as pivot
+    pivot = array[high]
+ 
+    # Pointer for greater element
+    i = low - 1
+ 
+    # Traverse through all elements
+    # compare each element with pivot
+    for j in range(low, high):
+        if array[j] <= pivot:
+            # If element smaller than pivot is found
+            # swap it with the greater element pointed by i
+            i = i + 1
+ 
+            # Swapping element at i with element at j
+            (array[i], array[j]) = (array[j], array[i])
+ 
+    # Swap the pivot element with
+    # e greater element specified by i
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+ 
+    # Return the position from where partition is done
+    return i + 1
+
 def quick_sort(data, low, high) -> List[int]:
     # Write code here
-
+    if low < high:
+        pi = partition(array, low, high)
+        quick_sort(array, low, pi - 1)
+        quick_sort(array, pi + 1, high)
+        return array
 
 input_data = input()
 data = []
